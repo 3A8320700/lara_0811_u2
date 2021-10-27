c943baaee1c1f1aed629dc76531eb64afc268ca1
LAPTOP-Q4E3DFIH\s9633 <s3a832070@student.ncut.edu.tw>
2021-10-27 10:36:36 +0800

    設定Route反應

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('r1', function () {
+    return view('r2');
+});
+Route::get('r2', function () {
+    return view('r3');
+});
+Route::get('r3', function () {
+    return view('welcome');
+});
